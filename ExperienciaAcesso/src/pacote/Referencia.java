711230d240646ef50644ce8cb9f69795c9378506
gleisson <gleissonfreitas@hotmail.com>
2016-05-19 11:15:00 -0300

    //Primeiro Commit

---
+++ ExperienciaAcesso/src/pacote/Referencia.java
@@ -0,0 +1,18 @@
+package pacote;
+
+public class Referencia {
+	
+	int modificadorDefault;
+	public int modificadorc;
+	public int modificadorPublic;
+	protected int modificadorProtegido;
+	private int modificadorPrivado;
+	
+	public void testeAcessoPropriaClasse(){
+		modificadorDefault = 0;
+		modificadorPublic = 0;
+		modificadorProtegido = 0;
+		modificadorPrivado = 0;
+	}
+	
+}
